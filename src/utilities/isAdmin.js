d67a5a8734c2e9c01dcdd71ef7aa5a22a3e91848
kepford <bob@bobkepford.com>
2018-01-31 20:50:36 -0800

    Makes ADMIN_USER ID an env variable

--- src/utilities/isAdmin.js
+++ src/utilities/isAdmin.js
@@ -2,6 +2,6 @@ import firebase from 'firebase';
 
 export default () => {
   const user = firebase.auth().currentUser;
-  const adminUid = 'YJ8P77ATwnVnWQvgo8Yy9i8bhPh1';
+  const adminUid = process.env.ADMIN_USER;
   return user.uid === adminUid ? true : false;
 };
