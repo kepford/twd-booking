ed63885069ebf575507239d81e51c05056d1d5fb
kepford <bob@bobkepford.com>
2018-03-03 18:25:16 -0800

    Only show future sponsorships and sort by date

--- src/selectors/sponsorships.js
+++ src/selectors/sponsorships.js
@@ -1,9 +1,14 @@
+import moment from 'moment';
+
 // Get sponsorships
 
-export default (sponsorships, { text }) => {
+export default (sponsorships) => {
   return sponsorships.filter((sponsorship) => {
-    const textMatch = sponsorship.issue.toLowerCase().includes(text.toLowerCase());
-
-    return textMatch;
+    const now = moment().valueOf();
+    const sponsorshipDate = sponsorship.date;
+    const future = sponsorshipDate > now ? true : false;
+    return future;
+  }).sort((a, b) => {
+    return a.date > b.date ? 1 : -1;
   });
 };
